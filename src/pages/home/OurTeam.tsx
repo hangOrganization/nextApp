f3c567dbb85555e5705e58b5669a5e178eca6e37
‘lllliii’ <9308381+lllliii111@user.noreply.gitee.com>
2023-06-14 21:24:45 +0800

    ui

--- src/pages/home/OurTeam.tsx
+++ src/pages/home/OurTeam.tsx
@@ -1,13 +1,32 @@
 import ourTeam_bg from '@/assets/image/svg/ourTeam-bg.svg'
-import icon_MA from '@/assets/image/svg/icon-MA.svg'
-import Image from 'next/image'
+import { teachers } from '@/utils/ourTeam'
 import styled from 'styled-components'
 
 const OurTeamBox = styled.div`
     width: 100%;
-    height: 913px;
+    margin-top: 450px;
     border-radius: 48px;
     background: url(${ourTeam_bg.src});
+ `
+const ButtonBox = styled.div`
+    cursor: pointer;
+    border: 1px solid #FFFFFF;
+    .hover{
+        border: 1px solid #FFFFFF; 
+    }
+    &:hover{
+        color: #FFFFFF;
+        background: #1A1A1A;
+        border: 1px solid #1A1A1A;
+        mix-blend-mode: difference;
+        .hover{
+            border: 1px solid #1A1A1A; 
+            background: #cccccc;
+        }
+        path{
+            stroke:#1A1A1A;
+        }
+    }
  `
 const OurTeamBg = styled.div`
     width: 100%;
@@ -18,15 +37,20 @@ const OurTeamBg = styled.div`
 const OurTeamBlur = styled.div`
     width: 100%;
     height: 100%;
+    padding-bottom: 80px;
     backdrop-filter: blur(250px);
     border-radius: 48px;
  `
 const CardBox = styled.div`
     width: 237px;
+    cursor: pointer;
     height: 368px;
-    background: url(${icon_MA.src});
     background-size: cover;
+    transition: all 150ms;
     background-repeat: no-repeat;
+    :hover{
+        transform: translateY(-10px);
+    }
  `
 const CardFilterBox = styled.div`
     position: relative;
@@ -35,15 +59,27 @@ const CardFilterBox = styled.div`
     background: linear-gradient(0deg, #ECECEC 10.27%, rgba(236, 236, 236, 0) 100%);
  `
 const NameOur = styled.p`
-    /* font-family: 'General Sans'; */
+    font-family: 'General Sans';
     font-style: italic;
     font-weight: 700;
     font-size: 36px;
     line-height: 120%;
     letter-spacing: 0.05em;
     text-transform: uppercase;
-    color: rgba(0, 0, 255, 0.01);
-    text-shadow: 2px 0px 0px #0000FF;
+    -webkit-text-stroke: 1px #0000FF;
+    text-shadow: 4px 0px 0px #0000FF;
+
+ `
+const RollBox = styled.div`
+    animation: teachersRoll 10s linear infinite;
+    @keyframes teachersRoll {
+        from {
+            transform: translateX(0px);
+        }
+        to {
+            transform: translateX(-1824px);
+        }
+    }
  `
 export default function OurTeam() {
     return (
@@ -70,44 +106,30 @@ export default function OurTeam() {
                             </div>
                         </div>
                     </div>
-                    <div className="flex gap-[67px] my-20">
-                        <CardBox>
-                            <CardFilterBox>
-                                <div className='p-6 pr-[17px] w-[253px] absolute bottom-0 right-[-51px] h-[184px] ml-[51px] bg-[#FFFFFF]'>
-                                    <NameOur>MA</NameOur>
-                                    <p className='font-normal text-[16px] mt-2 mb-4 leading-[120%] uppercase text-[#1a1a1a]'>音乐制作导师</p>
-                                    <p className='font-light text-[12px] leading-[180%] uppercase opacity-80 text-[#1a1a1a]'>  日本NHK电视台纪录片《平安京》作曲 中央电视台纪录片《家园》作曲</p>
-                                </div>
-                            </CardFilterBox>
-                        </CardBox>
-                        <CardBox>
-                            <CardFilterBox>
-                                <div className='p-6 pr-[17px] w-[253px] absolute bottom-0 right-[-51px] h-[184px] ml-[51px] bg-[#FFFFFF]'>
-                                    <NameOur>MA</NameOur>
-                                    <p className='font-normal text-[16px] mt-2 mb-4 leading-[120%] uppercase text-[#1a1a1a]'>音乐制作导师</p>
-                                    <p className='font-light text-[12px] leading-[180%] uppercase opacity-80 text-[#1a1a1a]'>  日本NHK电视台纪录片《平安京》作曲 中央电视台纪录片《家园》作曲</p>
-                                </div>
-                            </CardFilterBox>
-                        </CardBox>
-                        <CardBox>
-                            <CardFilterBox>
-                                <div className='p-6 pr-[17px] w-[253px] absolute bottom-0 right-[-51px] h-[184px] ml-[51px] bg-[#FFFFFF]'>
-                                    <NameOur>MA</NameOur>
-                                    <p className='font-normal text-[16px] mt-2 mb-4 leading-[120%] uppercase text-[#1a1a1a]'>音乐制作导师</p>
-                                    <p className='font-light text-[12px] leading-[180%] uppercase opacity-80 text-[#1a1a1a]'>  日本NHK电视台纪录片《平安京》作曲 中央电视台纪录片《家园》作曲</p>
-                                </div>
-                            </CardFilterBox>
-                        </CardBox>
+                    <div className="flex w-screen gap-[67px] my-20">
+                        <RollBox className='flex w-[3581px] gap-[67px]'>
+                            {[...teachers, ...teachers, ...teachers].map((el: any, index: number) => (
+                                <CardBox style={{ background: `url(${el.image.src})` }} key={`${el.name}-${index}-teachers`}>
+                                    <CardFilterBox>
+                                        <div className='p-6 pr-[17px] w-[253px] absolute bottom-0 right-[-51px] h-[184px] ml-[51px] bg-[#FFFFFF]'>
+                                            <NameOur>{el.name}</NameOur>
+                                            <p className='font-normal text-[16px] mt-2 mb-4 leading-[120%] uppercase text-[#1a1a1a]'>{el.position}</p>
+                                            <p className='font-light text-[12px] leading-[180%] uppercase opacity-80 text-[#1a1a1a]'>{el.works}</p>
+                                        </div>
+                                    </CardFilterBox>
+                                </CardBox>
+                            ))}
+                        </RollBox>
                     </div>
-                    <div style={{ border: "1px solid #FFFFFF" }} className='p-4 text-[16px] leading-[120%] w-[226px] font-normal mx-auto pl-[30px] pr-[15px] flex items-center rounded-[64px]'>
+                    <ButtonBox className='p-4 text-[16px] leading-[120%] w-[226px] font-normal mx-auto pl-[30px] pr-[15px] flex items-center rounded-[64px]'>
                         观看导师精彩作品
-                        <div style={{ border: "1px solid #FFFFFF" }} className='w-[36px] ml-[14px] items-center justify-center flex rounded-full h-[36px]'>
+                        <div className='w-[36px] hover ml-[14px] items-center justify-center flex rounded-full h-[36px]'>
                             <svg width="19" height="16" viewBox="0 0 19 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M9 15C9 15 11.9412 8 19 8C11.9412 8 9 1 9 1" stroke="white" stroke-width="1.6" />
-                                <path d="M18 8L0 8" stroke="white" stroke-width="1.6" />
+                                <path d="M9 15C9 15 11.9412 8 19 8C11.9412 8 9 1 9 1" stroke="white" strokeWidth="1.6" />
+                                <path d="M18 8L0 8" stroke="white" strokeWidth="1.6" />
                             </svg>
                         </div>
-                    </div>
+                    </ButtonBox>
                 </OurTeamBlur>
             </OurTeamBg>
         </OurTeamBox>
---
+++ src/utils/ourTeam.ts
@@ -0,0 +1,45 @@
+import MA from '@/assets/image/teachers-icon/icon-MA.svg'
+import Gao from '@/assets/image/teachers-icon/Gao.svg'
+import HUANG from '@/assets/image/teachers-icon/HUANG.svg'
+import XIA from '@/assets/image/teachers-icon/XIA.svg'
+import SHI from '@/assets/image/teachers-icon/SHI.svg'
+import ZHAO from '@/assets/image/teachers-icon/ZHAO.svg'
+
+export const teachers =[
+    {
+        name:'HUANG',
+        position:'审核导师',
+        works:'《我们的奥林匹克》《中国冰雪记忆》《世界遗产在中国》',
+        image:HUANG
+    },
+    {
+        name:'Gao',
+        position:'声音设计导师',
+        works:'《2018雅加达亚运会闭幕式杭州六分钟》《奔跑吧》《王牌对王牌》',
+        image:Gao
+    },
+    {
+        name:'MA',
+        position:'音乐制作导师',
+        works:'  日本NHK电视台纪录片《平安京》作曲 中央电视台纪录片《家园》作曲',
+        image:MA
+    },
+    {
+        name:'XIA',
+        position:'音乐产业及职业规划导师',
+        works:'《味道江南》《二炮手》《良渚情缘》《奔跑吧兄弟——天使的城》',
+        image:XIA
+    },
+    {
+        name:'SHI',
+        position:'声音设计/音乐编辑导师',
+        works:'《故事里的中国》《主持人大赛》《挑战不可能》《典籍里的中国》',
+        image:SHI
+    },
+    {
+        name:'ZHAO',
+        position:'万象循声创始人之一',
+        works:'《智联家园》《幸福进击》《寻找未来》《驾驭》《宇宙沙漏》《候鸟》',
+        image:ZHAO
+    },
+]
