b445094a282791ad6e2ca385c07a15ea0029b30d
Ganseblum <977252487@qq.com>
2023-06-14 23:47:32 +0800

    update

---
+++ src/pages/home/Disc.tsx
@@ -0,0 +1,13 @@
+"use client";
+import styled from "styled-components";
+import disc_bg from "../../assets/image/svg/disc-bg.svg";
+
+const Box = styled.div`
+  width: 100vw;
+  height: 1440px;
+  background: red;
+`;
+
+export default function Disc() {
+  return <Box></Box>;
+}
