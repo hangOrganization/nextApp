cfebe2888e23e832ce46716778ef2b42965619ab
Ganseblum <977252487@qq.com>
2023-06-15 23:24:36 +0800

    update

---
+++ src/utils/disc.List.ts
@@ -0,0 +1,3 @@
+import DOCUMENTARY from "../assets/image/disc-icon/DOCUMENTARY.svg";
+import RHYTHM from "../assets/image/disc-icon/RHYTHM.svg";
+import SCI_TECH from "../assets/image/disc-icon/SCI-TECH.svg";
