29839892cee8abd9905cf773488e526bba624162
Ganseblum <977252487@qq.com>
2023-06-24 15:20:47 +0800

    update

--- src/pages/home/Sign.tsx
+++ src/pages/home/Sign.tsx
@@ -10,107 +10,162 @@ import moireFringe_bg_1 from "@/assets/image/svg/moireFringe-bg-1.svg";
 import sign_bg_2 from "@/assets/image/svg/sign-bg-2.svg";
 import icon_star from "@/assets/image/svg/icon-star.svg";
 import lEtsRock_button from "@/assets/image/svg/lEt’s-Rock-button.svg";
-import { Box, ButtonBorder, ButtonBox, EchoRollBox, LineBox, MoireFringe, RollBox, 
-  ScaleBox, ShadowBox, ShadowBox2, SignBgBox, SingLogo, SliderBox, TextBox } from "./SignCss"
+import {
+  Box,
+  ButtonBorder,
+  ButtonBox,
+  EchoRollBox,
+  LineBox,
+  MoireFringe,
+  RollBox,
+  ScaleBox,
+  ShadowBox,
+  ShadowBox2,
+  SignBgBox,
+  SingLogo,
+  SliderBox,
+  TextBox,
+} from "./SignCss";
 import Video from "./Video";
+import { useEffect } from "react";
 interface SignProps {
   isOpenConsult: number;
   setIsOpenConsult: Function;
 }
+
 export default function Sign({ isOpenConsult, setIsOpenConsult }: SignProps) {
+  useEffect(() => {
+    console.log(2121212);
+  }, []);
+
   return (
-    <div className="relative w-screen">
-      <MoireFringe className="w-full top-0 flex z-30 justify-center items-center left-0 absolute h-[728px]">
-        <Image
-          className="absolute moireFringe  !w-screen !h-[1400px] z-30"
-          src={moireFringe_bg_1}
-          alt=""
-        />
-        <Image className="absolute z-20" src={moireFringe_bg} alt="" />
-      </MoireFringe>
-      <div className="md:h-[728px] z-40 relative">
-        <SignBgBox className=" absolute z-10 md:h-[728px] max-md:top-[100px] max-md:h-[324px] w-screen max-md:items-center flex justify-center top-0 left-0">
-          {/* <Image className='absolute  w-screen' src={sign_bg} alt='' /> */}
-          <Image className="absolute max-md:hidden sign-bg-1" src={sign_bg_1} alt="" />
-          <Image className="absolute md:hidden mt-10 sign-bg-1" src={mobile_sign_bg_1} alt="" />
-          <Image className="absolute md:hidden w-screen sign-bg-2" src={mobile_sign_bg_2} alt="" />
-          <Image className="absolute max-md:hidden w-screen sign-bg-2" src={sign_bg_2} alt="" />
-        </SignBgBox>
-        <div className=" relative z-20 flex justify-center items-center flex-col max-md:pt-[246px] pt-[302px] ">
-          <div className="h-[155px] max-md:h-[72px]">
-            <SingLogo className="h-[155px] max-md:h-[72px]">
-              <div className="img-box-1 max-md:h-[14.4px] h-[20px] overflow-hidden">
-                <Image className="h-[155px] max-md:h-[72px] " src={sign_logo} alt="" />
-              </div>
-              <div className="img-box-2 max-md:h-[14.4px] h-[50px] overflow-hidden">
-                <Image
-                  className="h-[155px] max-md:mt-[-14.4px] max-md:h-[72px] mt-[-20px] "
-                  src={sign_logo}
-                  alt=""
-                />
-              </div>
-              <div className="img-box-3 max-md:h-[14.4px] h-[30px] overflow-hidden">
-                <Image
-                  className="h-[155px] max-md:h-[72px] max-md:mt-[-28.8px] mt-[-70px] "
-                  src={sign_logo}
-                  alt=""
-                />
-              </div>
-              <div className="img-box-4 max-md:h-[14.4px]  h-[10px] overflow-hidden">
-                <Image
-                  className="h-[155px] max-md:h-[72px] max-md:mt-[-43.2px] mt-[-100px] "
-                  src={sign_logo}
-                  alt=""
-                />
-              </div>
-              <div className="img-box-5 max-md:h-[14.4px] h-[45px] overflow-hidden">
-                <Image
-                  className="h-[155px] max-md:h-[72px] max-md:mt-[-57.6px] mt-[-110px] "
-                  src={sign_logo}
-                  alt=""
-                />
-              </div>
-            </SingLogo>
-          </div>
-          <TextBox className="mt-[70px] max-md:mt-12 max-md:h-[21px] h-[49px]">
-            <div className="flex max-md:h-[21px] ">
-              <div className="w-[818px] max-md:w-[343px] max-md:h-[21px] h-[49px]">
-                <Image className=" w-[818px] max-md:w-[343px] max-md:h-[21px] h-[49px]" src={sign_text} alt="" />
-              </div>
+    <div className="relative w-screen a ">
+      <div>
+        <MoireFringe className="w-full top-0 flex z-30 justify-center items-center left-0 absolute h-[728px]">
+          <Image
+            className="absolute moireFringe  !w-screen !h-[1400px] z-30"
+            src={moireFringe_bg_1}
+            alt=""
+          />
+          {/* <Image className="absolute z-20" src={moireFringe_bg} alt="" /> */}
+        </MoireFringe>
+        <div className="md:h-[728px] z-40 relative">
+          <SignBgBox className=" absolute  z-10 md:h-[728px]   max-md:top-[100px] max-md:h-[324px] w-screen max-md:items-center flex justify-center top-0 left-0">
+            {/* <Image className='absolute  w-screen' src={sign_bg} alt='' /> */}
+            <Image
+              className="absolute max-md:hidden sign-bg-1"
+              src={sign_bg_1}
+              alt=""
+            />
+            <Image
+              className="absolute md:hidden mt-10 sign-bg-1"
+              src={mobile_sign_bg_1}
+              alt=""
+            />
+            <Image
+              className="absolute md:hidden w-screen sign-bg-2"
+              src={mobile_sign_bg_2}
+              alt=""
+            />
+            <Image
+              className="absolute max-md:hidden w-screen sign-bg-2"
+              src={sign_bg_2}
+              alt=""
+            />
+          </SignBgBox>
+          <div className=" relative z-20 flex justify-center items-center flex-col max-md:pt-[246px] pt-[302px] ">
+            <div className="h-[155px] max-md:h-[72px]">
+              <SingLogo className="h-[155px] max-md:h-[72px]">
+                <div className="img-box-1 max-md:h-[14.4px] h-[20px] overflow-hidden">
+                  <Image
+                    className="h-[155px] max-md:h-[72px] "
+                    src={sign_logo}
+                    alt=""
+                  />
+                </div>
+                <div className="img-box-2 max-md:h-[14.4px] h-[50px] overflow-hidden">
+                  <Image
+                    className="h-[155px] max-md:mt-[-14.4px] max-md:h-[72px] mt-[-20px] "
+                    src={sign_logo}
+                    alt=""
+                  />
+                </div>
+                <div className="img-box-3 max-md:h-[14.4px] h-[30px] overflow-hidden">
+                  <Image
+                    className="h-[155px] max-md:h-[72px] max-md:mt-[-28.8px] mt-[-70px] "
+                    src={sign_logo}
+                    alt=""
+                  />
+                </div>
+                <div className="img-box-4 max-md:h-[14.4px]  h-[10px] overflow-hidden">
+                  <Image
+                    className="h-[155px] max-md:h-[72px] max-md:mt-[-43.2px] mt-[-100px] "
+                    src={sign_logo}
+                    alt=""
+                  />
+                </div>
+                <div className="img-box-5 max-md:h-[14.4px] h-[45px] overflow-hidden">
+                  <Image
+                    className="h-[155px] max-md:h-[72px] max-md:mt-[-57.6px] mt-[-110px] "
+                    src={sign_logo}
+                    alt=""
+                  />
+                </div>
+                {/* <video
+                  className="mx-auto relative z-[1]"
+                  width="960"
+                  controls
+                  height="542"
+                >
+                  <source src="/video/sign.mp4" type="video/mp4"></source>
+                </video> */}
+              </SingLogo>
             </div>
-          </TextBox>
-          <div className="flex w-[780px] overflow-hidden">
-            <EchoRollBox className="flex">
-              <div className="w-[780px] text-center">
-                <p className=" font-normal uppercase tracking-[1em] mt-[72px] text-[26px]">
-                  听见世界的回响
-                </p>
+            <TextBox className="mt-[70px] max-md:mt-12 max-md:h-[21px] h-[49px]">
+              <div className="flex max-md:h-[21px] ">
+                <div className="w-[818px] max-md:w-[343px] max-md:h-[21px] h-[49px]">
+                  <Image
+                    className=" w-[818px] max-md:w-[343px] max-md:h-[21px] h-[49px]"
+                    src={sign_text}
+                    alt=""
+                  />
+                </div>
               </div>
-              <div className="w-[780px] text-center">
-                <p className=" font-[300] uppercase tracking-[0.51em] mt-[72px] font-[Lexend] text-[26px]">
-                  Echo of the World
-                </p>
-              </div>
-              <div className="w-[780px] text-center">
-                <p className=" font-normal uppercase tracking-[1em] mt-[72px] text-[26px]">
-                  听见世界的回响
-                </p>
-              </div>
-            </EchoRollBox>
+            </TextBox>
+            <div className="flex w-[780px] overflow-hidden">
+              <EchoRollBox className="flex">
+                <div className="w-[780px] text-center">
+                  <p className=" font-normal uppercase tracking-[1em] mt-[72px] text-[26px]">
+                    听见世界的回响
+                  </p>
+                </div>
+                <div className="w-[780px] text-center">
+                  <p className=" font-[300] uppercase tracking-[0.51em] mt-[72px] font-[Lexend] text-[26px]">
+                    Echo of the World
+                  </p>
+                </div>
+                <div className="w-[780px] text-center">
+                  <p className=" font-normal uppercase tracking-[1em] mt-[72px] text-[26px]">
+                    听见世界的回响
+                  </p>
+                </div>
+              </EchoRollBox>
+            </div>
           </div>
         </div>
       </div>
+
       <div className="flex items-center mt-[280px] px-[192px] justify-between">
         <div className="w-[224px] flex-col flex items-center">
           <LineBox className="w-full overflow-hidden">
             <div
               className="w-20 ml-[143px] line-1 h-[1px] mt-2"
-              style={{ border: "0.5px solid #CCCCCC" }}>
-            </div>
+              style={{ border: "0.5px solid #CCCCCC" }}
+            ></div>
             <div
               className="w-[65px] line-2 ml-[85px] h-[1px] mt-2"
-              style={{ border: "0.5px solid #CCCCCC" }}>
-            </div>
+              style={{ border: "0.5px solid #CCCCCC" }}
+            ></div>
             <div className="flex mt-[-2px] h-[86px] w-full gap-[6px] before justify-center items-end"></div>
             <div
               className="w-[62px] ml-[143px] line-3 h-[1px] mb-[8px]"
@@ -154,17 +209,13 @@ export default function Sign({ isOpenConsult, setIsOpenConsult }: SignProps) {
                   <p className="text-[32px] leading-[160%] font-[300]">
                     留学申请
                   </p>
-                  <p className="text-[32px] leading-[160%] font-[300]">
-                    规划
-                  </p>
+                  <p className="text-[32px] leading-[160%] font-[300]">规划</p>
                 </div>
                 <div className="text-center">
                   <p className="text-[32px] leading-[160%] font-[300]">
                     跨界艺术
                   </p>
-                  <p className="text-[32px] leading-[160%] font-[300]">
-                    联动
-                  </p>
+                  <p className="text-[32px] leading-[160%] font-[300]">联动</p>
                 </div>
                 <div className="text-center">
                   <p className="text-[32px] leading-[160%] font-[300]">
