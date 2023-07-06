465d8d37afe3ebf6aeda633f8181c6bea0a93267
lihongboQ <807053074@qq.com>
2023-07-07 05:19:52 +0800

    ui

--- src/components/Modal/CampusEnvironment.tsx
+++ src/components/Modal/CampusEnvironment.tsx
@@ -62,58 +62,19 @@ export default function CampusEnvironment({
                   closeModal()
                 }}
               >
-                <div className="w-[1200px] mx-auto items-center h-[520px] flex justify-center relative overflow-x-hidden">
-                  <div className="w-[644px] flex items-center h-[520px]">
-                    <div
-                      style={{
-                        transform: `translateX(-${currentPage * 668}px)`,
-                      }}
-                      className={`flex transition-all duration-500 gap-6`}
-                    >
-                      {list.map((el: any) => (
-                        <div
-                          key={`CampusEnvironment-${el.index}`}
-                          className="w-[644px] h-[520px]"
-                        >
-                          <Image
-                            className="w-[644px] object-contain h-[520px]"
-                            src={el.image}
-                            alt=""
-                          />
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                  {currentPage !== 0 && <div
-                    style={{
-                      background:
-                        "linear-gradient(270deg, rgba(0, 0, 0, 0) 0% , rgba(0, 0, 0, 0.9) 100%)",
-                    }}
-                    className=" w-52 h-full absolute top-0 left-0"
-                  ></div>}
-                  {currentPage !== list.length - 1 && <div
-                    style={{
-                      background:
-                        "linear-gradient(90deg, rgba(0, 0, 0, 0) 0% , rgba(0, 0, 0, 0.9) 100%)",
-                    }}
-                    className=" w-52 h-full absolute top-0 right-[-2px]"
-                  ></div>}
-                  <div className="w-[1200px] mx-auto items-center h-[520px] flex justify-center absolute top-0 left-0 overflow-x-hidden">
+                <div className="text-center max-md:hidden w-screen">
+                  <div className="w-[1200px] mx-auto items-center h-[520px] flex justify-center relative overflow-x-hidden">
                     <div className="w-[644px] flex items-center h-[520px]">
                       <div
                         style={{
                           transform: `translateX(-${currentPage * 668}px)`,
                         }}
-                        className={`flex opacity-0 transition-all duration-500 gap-6`}
+                        className={`flex transition-all duration-500 gap-6`}
                       >
                         {list.map((el: any) => (
                           <div
-                            onClick={(event) => {
-                              event.stopPropagation();
-                              setCurrentPage(el.index);
-                            }}
-                            key={`CampusEnvironment-${el.index}--`}
-                            className=" cursor-pointer w-[644px] h-[520px]"
+                            key={`CampusEnvironment-${el.index}`}
+                            className="w-[644px] h-[520px]"
                           >
                             <Image
                               className="w-[644px] object-contain h-[520px]"
@@ -124,88 +85,75 @@ export default function CampusEnvironment({
                         ))}
                       </div>
                     </div>
-                  </div>
-                </div>
-                <div className="relative flex justify-center">
-                  <div className=" relative flex justify-center items-center w-full">
-                    {list.map((el: any) => (
-                      <p key={`${el.index}-el.name-campusEnvironment`} className={`font-light absolute text-[14px] transition-all ${el.index===currentPage?'opacity-100':''} duration-1000 opacity-0 mt-8 leading-[180%]`}>
-                        {el.name}
-                      </p>
-                    ))}
-                  </div>
-                </div>
-                <div className="flex mt-[64px] justify-center relative w-[758px] overflow-hidden mx-auto items-center gap-4">
-                  <div className="flex w-[86px] items-center h-[86px] p-2">
-                    <div
-                      style={{
-                        transform: `translateX(-${currentPage * 86}px)`,
-                      }}
-                      className="flex transition-all duration-500 gap-4"
-                    >
-                      {list.map((el: any) => (
-                        <div
-                          key={`CampusEnvironment-${el.index}-min`}
-                          className="w-[70px] h-[70px]"
-                        >
-                          <Image
-                            className="w-[70px] h-[70px]"
-                            src={el.image}
-                            alt=""
-                            onClick={() => {
-                              if (currentPage !== oldCurrentPage) {
-                                setOldCurrentPage(currentPage);
-                              }
-                              setCurrentPage(el.index);
-                            }}
-                          />
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                  <div
-                    style={{ border: " 1px solid #FF4B00" }}
-                    className="flex w-[86px] absolute items-center h-[86px] p-2"
-                  ></div>
-                  {
-                    <div
+                    {currentPage !== 0 && <div
                       style={{
                         background:
                           "linear-gradient(270deg, rgba(0, 0, 0, 0) 0% , rgba(0, 0, 0, 0.9) 100%)",
                       }}
-                      className=" w-[300px] h-full absolute top-0 left-0"
-                    ></div>
-                  }
-                  <div
-                    style={{
-                      background:
-                        "linear-gradient(90deg, rgba(0, 0, 0, 0) 0% , rgba(0, 0, 0, 0.9) 100%)",
-                    }}
-                    className=" w-[300px] h-full absolute top-0 right-0"
-                  ></div>
-                  <div className="flex absolute justify-center w-[758px] overflow-hidden top-0 left-0 opacity-0 items-center gap-4">
-                    <div
-                      style={{ border: " 1px solid #FF4B00" }}
-                      className="flex w-[86px] items-center h-[86px] p-2"
-                    >
+                      className=" w-52 h-full absolute top-0 left-0"
+                    ></div>}
+                    {currentPage !== list.length - 1 && <div
+                      style={{
+                        background:
+                          "linear-gradient(90deg, rgba(0, 0, 0, 0) 0% , rgba(0, 0, 0, 0.9) 100%)",
+                      }}
+                      className=" w-52 h-full absolute top-0 right-[-2px]"
+                    ></div>}
+                    <div className="w-[1200px] mx-auto items-center h-[520px] flex justify-center absolute top-0 left-0 overflow-x-hidden">
+                      <div className="w-[644px] flex items-center h-[520px]">
+                        <div
+                          style={{
+                            transform: `translateX(-${currentPage * 668}px)`,
+                          }}
+                          className={`flex opacity-0 transition-all duration-500 gap-6`}
+                        >
+                          {list.map((el: any) => (
+                            <div
+                              onClick={(event) => {
+                                event.stopPropagation();
+                                setCurrentPage(el.index);
+                              }}
+                              key={`CampusEnvironment-${el.index}--`}
+                              className=" cursor-pointer w-[644px] h-[520px]"
+                            >
+                              <Image
+                                className="w-[644px] object-contain h-[520px]"
+                                src={el.image}
+                                alt=""
+                              />
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="relative flex justify-center">
+                    <div className=" relative flex justify-center items-center w-full">
+                      {list.map((el: any) => (
+                        <p key={`${el.index}-el.name-campusEnvironment`} className={`font-light absolute text-[14px] transition-all ${el.index === currentPage ? 'opacity-100' : ''} duration-1000 opacity-0 mt-8 leading-[180%]`}>
+                          {el.name}
+                        </p>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="flex mt-[64px] justify-center relative w-[758px] overflow-hidden mx-auto items-center gap-4">
+                    <div className="flex w-[86px] items-center h-[86px] p-2">
                       <div
                         style={{
                           transform: `translateX(-${currentPage * 86}px)`,
                         }}
-                        className="flex cursor-pointer transition-all duration-500 gap-4"
+                        className="flex transition-all duration-500 gap-4"
                       >
-                        {" "}
                         {list.map((el: any) => (
                           <div
-                            key={`CampusEnvironment-${el.index}-min---`}
+                            key={`CampusEnvironment-${el.index}-min`}
                             className="w-[70px] h-[70px]"
                           >
                             <Image
                               className="w-[70px] h-[70px]"
                               src={el.image}
                               alt=""
-                              onClick={(event) => {
-                                event.stopPropagation()
+                              onClick={() => {
                                 if (currentPage !== oldCurrentPage) {
                                   setOldCurrentPage(currentPage);
                                 }
@@ -216,11 +164,77 @@ export default function CampusEnvironment({
                         ))}
                       </div>
                     </div>
+                    <div
+                      style={{ border: " 1px solid #FF4B00" }}
+                      className="flex w-[86px] absolute items-center h-[86px] p-2"
+                    ></div>
+                    {
+                      <div
+                        style={{
+                          background:
+                            "linear-gradient(270deg, rgba(0, 0, 0, 0) 0% , rgba(0, 0, 0, 0.9) 100%)",
+                        }}
+                        className=" w-[300px] h-full absolute top-0 left-0"
+                      ></div>
+                    }
+                    <div
+                      style={{
+                        background:
+                          "linear-gradient(90deg, rgba(0, 0, 0, 0) 0% , rgba(0, 0, 0, 0.9) 100%)",
+                      }}
+                      className=" w-[300px] h-full absolute top-0 right-0"
+                    ></div>
+                    <div className="flex absolute justify-center w-[758px] overflow-hidden top-0 left-0 opacity-0 items-center gap-4">
+                      <div
+                        style={{ border: " 1px solid #FF4B00" }}
+                        className="flex w-[86px] items-center h-[86px] p-2"
+                      >
+                        <div
+                          style={{
+                            transform: `translateX(-${currentPage * 86}px)`,
+                          }}
+                          className="flex cursor-pointer transition-all duration-500 gap-4"
+                        >
+                          {" "}
+                          {list.map((el: any) => (
+                            <div
+                              key={`CampusEnvironment-${el.index}-min---`}
+                              className="w-[70px] h-[70px]"
+                            >
+                              <Image
+                                className="w-[70px] h-[70px]"
+                                src={el.image}
+                                alt=""
+                                onClick={(event) => {
+                                  event.stopPropagation()
+                                  if (currentPage !== oldCurrentPage) {
+                                    setOldCurrentPage(currentPage);
+                                  }
+                                  setCurrentPage(el.index);
+                                }}
+                              />
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <p className="mt-4 font-light text-[#FF4B00] text-[14px] leading-[180%]">
+                    {currentPage + 1}/{list.length}
+                  </p>
+                </div>
+                <div className="md:hidden">
+                  <div className=" flex justify-between gap-y-4 flex-wrap">
+                    {list.map((el: any) => (
+                      <div className=" w-[165px] relative flex justify-center items-end h-[160px]" key={`mobile-CampusEnvironment-${el.index}`} >
+                        <p className="mb-2 font-light absolute text-[12px] leading-[180%]">
+                          {el.name}
+                        </p>
+                        <Image src={el.image} className="w-[165px] h-[165px]" alt="" />
+                      </div>
+                    ))}
                   </div>
                 </div>
-                <p className="mt-4 font-light text-[#FF4B00] text-[14px] leading-[180%]">
-                  {currentPage + 1}/{list.length}
-                </p>
               </Dialog.Panel>
             </Transition.Child>
           </div>
