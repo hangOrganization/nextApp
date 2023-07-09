0f4e114771094f6210808f82bf41187def689ad3
Ganseblum <977252487@qq.com>
2023-07-09 13:28:56 +0800

    update

--- src/components/Modal/CampusEnvironment.tsx
+++ src/components/Modal/CampusEnvironment.tsx
@@ -60,19 +60,27 @@ export default function CampusEnvironment({
               <Dialog.Panel
                 className="text-center w-screen"
                 onClick={() => {
-                  closeModal()
+                  closeModal();
                 }}
               >
                 <Dialog.Title
                   as="h3"
-                  className="text-lg md:hidden flex justify-end font-medium leading-6 text-gray-900"
+                  className="text-lg md:hidden flex justify-end font-medium leading-6 text-gray-900 max-md:absolute max-md:right-0"
                 >
-                  <svg onClick={closeModal} className=' cursor-pointer' width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <svg
+                    onClick={closeModal}
+                    className=" cursor-pointer"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 32 32"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path d="M8 24L24.0012 7.99879" stroke="#ffffff" />
                     <path d="M8 8L24.0012 24.0012" stroke="#ffffff" />
                   </svg>
                 </Dialog.Title>
-                <div className="text-center max-md:hidden w-screen">
+                <div className="text-center max-md:hidden w-screen max-md:pt-[32px]">
                   <div className="w-[1200px] mx-auto items-center h-[520px] flex justify-center relative overflow-x-hidden">
                     <div className="w-[644px] flex items-center h-[520px]">
                       <div
@@ -95,20 +103,24 @@ export default function CampusEnvironment({
                         ))}
                       </div>
                     </div>
-                    {currentPage !== 0 && <div
-                      style={{
-                        background:
-                          "linear-gradient(270deg, rgba(0, 0, 0, 0) 0% , rgba(0, 0, 0, 0.9) 100%)",
-                      }}
-                      className=" w-52 h-full absolute top-0 left-0"
-                    ></div>}
-                    {currentPage !== list.length - 1 && <div
-                      style={{
-                        background:
-                          "linear-gradient(90deg, rgba(0, 0, 0, 0) 0% , rgba(0, 0, 0, 0.9) 100%)",
-                      }}
-                      className=" w-52 h-full absolute top-0 right-[-2px]"
-                    ></div>}
+                    {currentPage !== 0 && (
+                      <div
+                        style={{
+                          background:
+                            "linear-gradient(270deg, rgba(0, 0, 0, 0) 0% , rgba(0, 0, 0, 0.9) 100%)",
+                        }}
+                        className=" w-52 h-full absolute top-0 left-0"
+                      ></div>
+                    )}
+                    {currentPage !== list.length - 1 && (
+                      <div
+                        style={{
+                          background:
+                            "linear-gradient(90deg, rgba(0, 0, 0, 0) 0% , rgba(0, 0, 0, 0.9) 100%)",
+                        }}
+                        className=" w-52 h-full absolute top-0 right-[-2px]"
+                      ></div>
+                    )}
                     <div className="w-[1200px] mx-auto items-center h-[520px] flex justify-center absolute top-0 left-0 overflow-x-hidden">
                       <div className="w-[644px] flex items-center h-[520px]">
                         <div
@@ -140,7 +152,12 @@ export default function CampusEnvironment({
                   <div className="relative flex justify-center">
                     <div className=" relative flex justify-center items-center w-full">
                       {list.map((el: any) => (
-                        <p key={`${el.index}-el.name-campusEnvironment`} className={`font-light absolute text-[14px] transition-all ${el.index === currentPage ? 'opacity-100' : ''} duration-1000 opacity-0 mt-8 leading-[180%]`}>
+                        <p
+                          key={`${el.index}-el.name-campusEnvironment`}
+                          className={`font-light absolute text-[14px] transition-all ${
+                            el.index === currentPage ? "opacity-100" : ""
+                          } duration-1000 opacity-0 mt-8 leading-[180%]`}
+                        >
                           {el.name}
                         </p>
                       ))}
@@ -216,7 +233,7 @@ export default function CampusEnvironment({
                                 src={el.image}
                                 alt=""
                                 onClick={(event) => {
-                                  event.stopPropagation()
+                                  event.stopPropagation();
                                   if (currentPage !== oldCurrentPage) {
                                     setOldCurrentPage(currentPage);
                                   }
@@ -234,22 +251,35 @@ export default function CampusEnvironment({
                   </p>
                 </div>
                 <div className="md:hidden mt-4">
-                  <div className=" flex justify-between gap-y-4 flex-wrap">
+                  <div className=" flex justify-between gap-y-[6px] flex-wrap">
                     {list.map((el: any) => (
-                      <div onClick={(event) => {
-                        event.stopPropagation()
-                        setCurrentPage(el.index)
-                        setOpen(1)
-                      }} className=" w-[165px] relative flex justify-center items-end h-[160px]" key={`mobile-CampusEnvironment-${el.index}`} >
-                        <p className="mb-2 font-light absolute text-ellipsis overflow-hidden line-clamp-1 text-[12px] leading-[180%]">
+                      <div
+                        onClick={(event) => {
+                          event.stopPropagation();
+                          setCurrentPage(el.index);
+                          setOpen(1);
+                        }}
+                        className=" w-[165px] relative flex justify-center items-end h-[160px] max-md:h-[190px]"
+                        key={`mobile-CampusEnvironment-${el.index}`}
+                      >
+                        <p className="mb-2 font-light absolute text-ellipsis overflow-hidden line-clamp-1 text-[12px] max-md:bottom-[-32px] leading-[180%]">
                           {el.name}
                         </p>
-                        <Image src={el.image} className="w-[165px] object-contain h-[165px]" alt="" />
+                        <Image
+                          src={el.image}
+                          className="w-[165px] object-cover h-[165px]"
+                          alt=""
+                        />
                       </div>
                     ))}
                   </div>
                 </div>
-                <Modal isOpen={open} setCurrentPage={setCurrentPage} currentPage={currentPage} setIsOpen={setOpen} />
+                <Modal
+                  isOpen={open}
+                  setCurrentPage={setCurrentPage}
+                  currentPage={currentPage}
+                  setIsOpen={setOpen}
+                />
               </Dialog.Panel>
             </Transition.Child>
           </div>
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -20,68 +20,75 @@ const TurningButton = styled.div`
   }
 `;
 interface CampusEnvironmentProps {
-    isOpen: number;
-    currentPage: number;
-    setCurrentPage: Function;
-    setIsOpen: Function;
+  isOpen: number;
+  currentPage: number;
+  setCurrentPage: Function;
+  setIsOpen: Function;
 }
 export default function Modal({
-    currentPage,
-    setCurrentPage,
-    isOpen,
-    setIsOpen,
+  currentPage,
+  setCurrentPage,
+  isOpen,
+  setIsOpen,
 }: CampusEnvironmentProps) {
-    function closeModal() {
-        setIsOpen(0);
-    }
-    return (
-        <Transition appear show={isOpen === 1 ? true : false} as={Fragment}>
-            <Dialog as="div" className="relative z-[1000000]" onClose={closeModal}>
-                <Transition.Child
-                    as={Fragment}
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
-                    <div className="fixed inset-0 bg-black bg-opacity-90" />
-                </Transition.Child>
-                <div className="fixed inset-0 overflow-y-auto">
-                    <div className="flex min-h-full items-center justify-center text-center">
-                        <Transition.Child
-                            as={Fragment}
-                            enter="ease-out duration-300"
-                            enterFrom="opacity-0 scale-95"
-                            enterTo="opacity-100 scale-100"
-                            leave="ease-in duration-200"
-                            leaveFrom="opacity-100 scale-100"
-                            leaveTo="opacity-0 scale-95"
-                        >
-                            <Dialog.Panel
-                                className="text-center w-screen"
-                                onClick={() => {
-                                    closeModal()
-                                }}
-                            >
-                                <Swiper
-                                initialSlide={currentPage}
-                                className="mySwiper">
-                                    {list.map((el) => (
-                                        <SwiperSlide className=" flex justify-center items-center" key={`${el.index}--mySwiper`}>
-                                            <div className="flex relative h-screen w-screen justify-center items-center">
-                                                <Image className="flex justify-center items-center" src={el.image} alt="" />
-                                            </div>
-                                            <p className="absolute left-4 bottom-8 font-light text-ellipsis overflow-hidden line-clamp-1 text-[20px] leading-[180%]">{el.name}</p>
-                                        </SwiperSlide>))
-                                    }
-                                </Swiper>
-                            </Dialog.Panel>
-                        </Transition.Child>
-                    </div>
-                </div>
-            </Dialog>
-        </Transition>
-    );
+  function closeModal() {
+    setIsOpen(0);
+  }
+  return (
+    <Transition appear show={isOpen === 1 ? true : false} as={Fragment}>
+      <Dialog as="div" className="relative z-[1000000]" onClose={closeModal}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-90" />
+        </Transition.Child>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel
+                className="text-center w-screen"
+                onClick={() => {
+                  closeModal();
+                }}
+              >
+                <Swiper initialSlide={currentPage} className="mySwiper">
+                  {list.map((el) => (
+                    <SwiperSlide
+                      className=" flex justify-center items-center"
+                      key={`${el.index}--mySwiper`}
+                    >
+                      <div className="flex relative h-screen w-screen justify-center items-center">
+                        <Image
+                          className="flex justify-center items-center"
+                          src={el.image}
+                          alt=""
+                        />
+                      </div>
+                      <p className="absolute left-4 bottom-8 font-light text-ellipsis overflow-hidden line-clamp-1 text-[14px] leading-[180%]">
+                        {el.name}
+                      </p>
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
 }
