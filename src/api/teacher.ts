1dcf0490e4926dc7360ea47ee2a7b4fe650d553e
lijieQAQ <lijie1319722045@126.com>
2023-08-13 22:35:33 +0800

    fix: update tutor method

--- src/api/teacher.ts
+++ src/api/teacher.ts
@@ -1,10 +1,10 @@
-export const baseUrl = "http://192.168.0.107:8080";
+export const baseUrl = "http://182.92.217.183:9000";
 // export const baseUrl = "http://localhost:8080";
 
 export async function getTeacherList() {
   try {
     const res = await fetch(baseUrl + "/tutor/allTutors", {
-      method: "GET",
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
@@ -12,6 +12,6 @@ export async function getTeacherList() {
     const { data } = await res.json();
     return data;
   } catch {
-    alert("网络错误");
+    // alert("网络错误");
   }
 }
