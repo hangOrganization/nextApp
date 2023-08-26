ad19affc6726ffd93d78cfa4eb3698eaaf62c642
lihongboQ <807053074@qq.com>
2023-08-26 15:36:37 +0800

    updata

--- src/api/teacher.ts
+++ src/api/teacher.ts
@@ -1,5 +1,5 @@
-export const baseUrl = "http://192.168.0.107:8080";
-// export const baseUrl = "http://localhost:8080";
+// export const baseUrl = "http://192.168.0.107:8080";
+export const baseUrl = "http://localhost:8080";
 
 export async function getTeacherList() {
   try {
